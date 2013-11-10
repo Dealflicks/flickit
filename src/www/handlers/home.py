9a7a432f6ebd7594852e74a7c5cf867da50f88dd
Zachary Cancio <zach@dealflicks.com>
2013-11-10 13:13:47 -0800

    Move navbar to base.   Don’t get flicks if user is none on home.

--- src/www/handlers/home.py
+++ src/www/handlers/home.py
@@ -6,10 +6,14 @@ import models.movie
 class HomeHandler(handlers.base.BaseHandler):
 
     def get(self):
-		user = self.get_current_user()
-		flicks = user.flicks
-		golden_movies = models.movie.MovieModel.get_golden_movies(self.application)
-		self.render("home.html", user=user, flicks=flicks, movies=golden_movies)
+        user = self.get_current_user()
+
+        flicks = []
+        if user:
+          flicks = user.flicks
+
+        golden_movies = models.movie.MovieModel.get_golden_movies(self.application)
+        self.render("home.html", user=user, flicks=flicks, movies=golden_movies)
 
 
 
