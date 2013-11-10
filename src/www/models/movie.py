7e0608af72fe18e4e984062abdc4874f872b2902
Matthew Edwards <matthew.ed@gmail.com>
2013-11-10 10:25:37 -0800

    Changed up the movie model to add property’s fixed one or two things in flicks.html

--- src/www/models/movie.py
+++ src/www/models/movie.py
@@ -13,6 +13,17 @@ class MovieModel(models.base.BaseModel):
         self._app = app
         self._id = utils.utils.get_attr_or_default(init_dict, 'id')
         self._name = utils.utils.get_attr_or_default(init_dict, 'name')
+        self._dealflicks_id = utils.utils.get_attr_or_default(init_dict, 'dealflicks_id')
+        self._poster = utils.utils.get_attr_or_default(init_dict, 'poster')
+        self._critics_score = utils.utils.get_attr_or_default(init_dict, 'critics_score')
+        self._audience_score = utils.utils.get_attr_or_default(init_dict, 'audience_score')
+        self._duration = utils.utils.get_attr_or_default(init_dict, 'duration')
+        self._mpaa_rating = utils.utils.get_attr_or_default(init_dict, 'mpaa_rating')
+        self._genre = utils.utils.get_attr_or_default(init_dict, 'genre')
+        self._abridged_cast = utils.utils.get_attr_or_default(init_dict, 'abridged_cast')
+        self._synopsis = utils.utils.get_attr_or_default(init_dict, 'synopsis')
+        self._theater_release_date = utils.utils.get_attr_or_default(init_dict, 'theater_release_date')
+        self._rotten_tomatoes_id = utils.utils.get_attr_or_default(init_dict, 'rotten_tomatoes_id')
 
     ### Properties ###
     @property
@@ -22,6 +33,50 @@ class MovieModel(models.base.BaseModel):
     @property
     def name(self):
         return self._name
+
+    @property
+    def dealflicks_id(self):
+        return self._dealflicks_id
+
+    @property
+    def poster(self):
+        return self._poster
+
+    @property
+    def critics_score(self):
+        return self._critics_score
+
+    @property
+    def audience_score(self):
+        return self._audience_score
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @property
+    def mpaa_rating(self):
+        return self._mpaa_rating
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @property
+    def abridged_cast(self):
+        return self._abridged_cast
+
+    @property
+    def synopsis(self):
+        return self._synopsis
+
+    @property
+    def theater_release_date(self):
+        return self._theater_release_date
+
+    @property
+    def rotten_tomatoes_id(self):
+        return self._rotten_tomatoes_id
 
     @property
     def count(self):
