ad67195018bd7038467b8b7c40bc21e4a2225dc5
Matthew Edwards <matthew.ed@gmail.com>
2013-11-10 08:36:36 -0800

    do straight image search for search by image instead of editing

--- src/www/handlers/imagesearch.py
+++ src/www/handlers/imagesearch.py
@@ -20,9 +20,6 @@ class PictureHandler(handlers.base.BaseHandler):
         return (img_text, img_url, key_name, edit_count)
 
     def search_for_image(self, org_img_url, key_name=None):
-    	if (self.errors):
-            return self.send_error(400, chunk={'Status' : 'Error', 'Errors' : self.errors })
-
         img_url = org_img_url
         img_text, img_url, key_name, edit_count = self.cycle_the_filter(img_url, key_name, 0)   
 
@@ -52,7 +49,9 @@ class PictureSearchHandler(PictureHandler):
 
     def post(self):
     	org_img_url = self.valid('imgurl', str, required=True)
-    	id, name, nltk = self.search_for_image(org_img_url)
+    	if (self.errors):
+            return self.send_error(400, chunk={'Status' : 'Error', 'Errors' : self.errors })
+    	id, name, nltk = self.search_for_picture(org_img_url)
     	self.write("movie_id: %s" % id)
 
 
@@ -60,14 +59,31 @@ class PictureSearchByStringHandler(PictureHandler):
 
     def post(self):
     	img_string = self.valid('imgstr', str, required=True)
+    	if (self.errors):
+            return self.send_error(400, chunk={'Status' : 'Error', 'Errors' : self.errors })
     	imgbuf = StringIO()
     	imgbuf.write(img_string)
     	org_img_url, key_name = utils.utils.upload_image(imgbuf)
-    	id, name, nltk = self.search_for_image(org_img_url, key_name)
+    	id, name, nltk = self.search_for_picture(org_img_url, key_name)
     	self.write("movie_id: %s" % id)   
 
-class JSONPHandler(PictureHandler):
-    
+class JSONPHandler(handlers.base.BaseHandler):
+
+    def search_for_image(self, img_url, key_name=None):
+        img_text = utils.utils.get_best_for_image(img_url)   
+
+        if img_text == False:
+        	return self.send_error(400, chunk={'Status' : 'Error', 'Errors' : self.errors })
+
+        movies = self.mysqldb.query("SELECT id, name FROM movie ORDER BY name ASC")
+
+        our_movie = utils.utils.get_movie_for_best_guess(movies, img_text)
+        movie_id = our_movie['id']
+        movie_name = our_movie['movie']
+        movie_nltk = our_movie['nltk']
+    	
+    	return (movie_id, movie_name, movie_nltk)
+
     def write(self, callback, stuff):
         super(JSONPHandler, self).write(callback + '(' + tornado.escape.json_encode(stuff) + ')')
         self.set_header('Content-Type', 'application/javascript')
